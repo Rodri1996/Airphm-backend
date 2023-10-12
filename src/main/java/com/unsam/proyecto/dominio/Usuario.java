598aad2133a3748825de10257538234118265854
Rodri1996 <manunieto015@gmail.com>
2023-10-12 16:41:01 -0300

    se agregan las publicaciones y las reservas en la clase Usuario

--- src/main/java/com/unsam/proyecto/dominio/Usuario.java
+++ src/main/java/com/unsam/proyecto/dominio/Usuario.java
@@ -1,6 +1,10 @@
 package com.unsam.proyecto.dominio;
 
+import java.util.ArrayList;
+
 public class Usuario {
 
-
+	protected ArrayList<Usuario> amigos = new ArrayList<Usuario>();
+	protected ArrayList<Reserva> reservas = new ArrayList<Reserva>();
+	protected ArrayList<Hospedaje> publicaciones = new ArrayList<Hospedaje>();
 }
